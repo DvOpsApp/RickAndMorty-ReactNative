a76078477bb76643db41c8957faebd249bac1c77
Camilo Alaniz <kmilo93sd@gmail.com>
2020-08-31 00:31:39 -0400

    added screens Home and Details

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import Home from './screens/home';
+import HomeScreen from './screens/home';
+import DetailsScreen from './screens/details';
 
 const Stack = createStackNavigator();
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="home">
-        <Stack.Screen name={'home'} component={Home} />
+        <Stack.Screen name={'home'} component={HomeScreen} />
+        <Stack.Screen name={'details'} component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
